3e0d98da5e1bbfaf052e2e4ba9c0756a39b5c75d
Anmol Sahoo <anmol.sahoo25@gmail.com>
2018-06-28 18:46:18 +0530

    Added setup.py and configured __version__.py

---
+++ setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
+
+# Package meta-data
+NAME='aapg'
+DESCRIPTION = 'Automatic Assembly Generator for RISC-V'
+AUTHOR = 'Nandu Raj P'
+EMAIL = 'shakti@iitm.ac.in'
+VERSION = None
+
+REQUIRED = [
+]
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with io.open(os.path.join(here, 'README.md'), encoding = 'utf-8') as f:
+    long_description = '\n' + f.read()
+
+if not VERSION:
+    with io.open(os.path.join(here, NAME, '__version__.py')) as f:
+        VERSION = f.read()
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    packages=find_packages(exclude=('tests',)),
+    entry_points={
+        'console_scripts': ['aapg=aapg.main:execute'],
+    },
+    install_requires=REQUIRED,
+    license='MIT',
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+)
