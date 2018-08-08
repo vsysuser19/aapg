378880a71d7371c358fb74e094d966f50d7e82c1
Anmol Sahoo <anmol.sahoo25@gmail.com>
2018-08-08 19:08:02 +0530

    Removed prelude template for now

--- aapg/asm_templates.py
+++ aapg/asm_templates.py
@@ -5,9 +5,6 @@
 def prelude_template(args):
     stack_size = args['stack_size']
     return [
-	('addi', 'sp', 'sp', '-{}'.format(stack_size)),
-	('sd', 's0', 'sp', '{}'.format(stack_size - 8)),
-	('addi', 's0', 'sp', '{}'.format(stack_size))
     ]
 
 def recurse_sections():
