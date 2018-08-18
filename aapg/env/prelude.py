4629341d1cde83797e9b205218c1b9982b0e273d
Anmol Sahoo <anmol.sahoo25@gmail.com>
2018-08-18 14:26:37 +0530

    Updated prelude to load random data into registers

--- aapg/env/prelude.py
+++ aapg/env/prelude.py
@@ -1,52 +1,54 @@
 crt_asm = '''
-# See LICENSE for license details.
-
 #include "encoding.h"
 
 #if __riscv_xlen == 64
 # define LREG ld
 # define SREG sd
 # define REGBYTES 8
+# define FLREG fld
+# define FSREG fsd
 #else
 # define LREG lw
 # define SREG sw
 # define REGBYTES 4
+# define FLREG flw
+# define FSREG fsw
 #endif
 
   .section ".text.init"
   .globl _start
 _start:
-  li  x1, 0
-  li  x2, 0
-  li  x3, 0
-  li  x4, 0
-  li  x5, 0
-  li  x6, 0
-  li  x7, 0
-  li  x8, 0
-  li  x9, 0
-  li  x10,0
-  li  x11,0
-  li  x12,0
-  li  x13,0
-  li  x14,0
-  li  x15,0
-  li  x16,0
-  li  x17,0
-  li  x18,0
-  li  x19,0
-  li  x20,0
-  li  x21,0
-  li  x22,0
-  li  x23,0
-  li  x24,0
-  li  x25,0
-  li  x26,0
-  li  x27,0
-  li  x28,0
-  li  x29,0
-  li  x30,0
-  li  x31,0
+  la sp, data
+  LREG  x1, 0*REGBYTES(sp)
+  LREG  x3, 1*REGBYTES(sp)
+  LREG  x4, 2*REGBYTES(sp)
+  LREG  x5, 3*REGBYTES(sp)
+  LREG  x6, 4*REGBYTES(sp)
+  LREG  x7, 5*REGBYTES(sp)
+  LREG  x8, 6*REGBYTES(sp)
+  LREG  x9, 7*REGBYTES(sp)
+  LREG  x10,8*REGBYTES(sp)
+  LREG  x11,9*REGBYTES(sp)
+  LREG  x12,10*REGBYTES(sp)
+  LREG  x13,11*REGBYTES(sp)
+  LREG  x14,12*REGBYTES(sp)
+  LREG  x15,13*REGBYTES(sp)
+  LREG  x16,14*REGBYTES(sp)
+  LREG  x17,15*REGBYTES(sp)
+  LREG  x18,16*REGBYTES(sp)
+  LREG  x19,17*REGBYTES(sp)
+  LREG  x20,18*REGBYTES(sp)
+  LREG  x21,19*REGBYTES(sp)
+  LREG  x22,20*REGBYTES(sp)
+  LREG  x23,21*REGBYTES(sp)
+  LREG  x24,22*REGBYTES(sp)
+  LREG  x25,23*REGBYTES(sp)
+  LREG  x26,24*REGBYTES(sp)
+  LREG  x27,25*REGBYTES(sp)
+  LREG  x28,26*REGBYTES(sp)
+  LREG  x29,27*REGBYTES(sp)
+  LREG  x30,28*REGBYTES(sp)
+  LREG  x31,29*REGBYTES(sp)
 
   # enable FPU and accelerator if present
   li t0, MSTATUS_FS | MSTATUS_XS
@@ -72,38 +74,37 @@ _start:
   csrw mtvec, t0
 
   fssr    x0
-  fmv.s.x f0, x0
-  fmv.s.x f1, x0
-  fmv.s.x f2, x0
-  fmv.s.x f3, x0
-  fmv.s.x f4, x0
-  fmv.s.x f5, x0
-  fmv.s.x f6, x0
-  fmv.s.x f7, x0
-  fmv.s.x f8, x0
-  fmv.s.x f9, x0
-  fmv.s.x f10,x0
-  fmv.s.x f11,x0
-  fmv.s.x f12,x0
-  fmv.s.x f13,x0
-  fmv.s.x f14,x0
-  fmv.s.x f15,x0
-  fmv.s.x f16,x0
-  fmv.s.x f17,x0
-  fmv.s.x f18,x0
-  fmv.s.x f19,x0
-  fmv.s.x f20,x0
-  fmv.s.x f21,x0
-  fmv.s.x f22,x0
-  fmv.s.x f23,x0
-  fmv.s.x f24,x0
-  fmv.s.x f25,x0
-  fmv.s.x f26,x0
-  fmv.s.x f27,x0
-  fmv.s.x f28,x0
-  fmv.s.x f29,x0
-  fmv.s.x f30,x0
-  fmv.s.x f31,x0
+  fmv.w.x  f1, x1
+  fmv.w.x  f2, x2
+  fmv.w.x  f3, x3
+  fmv.w.x  f4, x4
+  fmv.w.x  f5, x5
+  fmv.w.x  f6, x6
+  fmv.w.x  f7, x7
+  fmv.w.x  f8, x8
+  fmv.w.x  f9, x9
+  fmv.w.x  f10, x10
+  fmv.w.x  f11, x11
+  fmv.w.x  f12, x12
+  fmv.w.x  f13, x13
+  fmv.w.x  f14, x14
+  fmv.w.x  f15, x15
+  fmv.w.x  f16, x16
+  fmv.w.x  f17, x17
+  fmv.w.x  f18, x18
+  fmv.w.x  f19, x19
+  fmv.w.x  f20, x20
+  fmv.w.x  f21, x21
+  fmv.w.x  f22, x22
+  fmv.w.x  f23, x23
+  fmv.w.x  f24, x24
+  fmv.w.x  f25, x25
+  fmv.w.x  f26, x26
+  fmv.w.x  f27, x27
+  fmv.w.x  f28, x28
+  fmv.w.x  f29, x29
+  fmv.w.x  f30, x30
+  fmv.w.x  f31, x31
 1:
 #endif
 
